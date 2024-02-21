bb7af64ddbfecd4a3176b4067d75015fbd83064f
tymenpay <147150471+tymenpay@users.noreply.github.com>
2024-02-21 22:08:51 +0300

    palindrome

--- leetc/palindrome2.c
+++ leetc/palindrome2.c
@@ -1,23 +1,23 @@
-#include <stdio.h>
 #include <stdbool.h>
+#include <stdio.h>
 bool isPalindrome(int x);
 
-int main(){
+int main() {
     int x;
     scanf("%d", &x);
     printf("%d", isPalindrome(x));
-    
-    return 0;;
+
+    return 0;
 }
-bool isPalindrome(int x){
-    if(x < 0){
+bool isPalindrome(int x) {
+    if (x < 0) {
         return false;
     }
     long x2 = x;
     long result = 0;
     int length = snprintf("NULL", 0, "%d", x);
-    for(int i = 0; i < length; i++, x /= 10){
-        result =  result * 10 + (x % 10);
+    for (int i = 0; i < length; i++, x /= 10) {
+        result = result * 10 + (x % 10);
     }
     return x2 == result;
 }
